99247e11aeba4ee6e753d38843b4a626f59ef2c8
KJ <kangjing324@live.com>
2021-10-15 01:58:45 +0800

    KJ update

--- app/src/main/java/com/example/healthtracker/login/LoginActivity.kt
+++ app/src/main/java/com/example/healthtracker/login/LoginActivity.kt
@@ -5,9 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.healthtracker.R
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.layout_forget_password.view.*
 
 class LoginActivity : AppCompatActivity()  {
 
@@ -25,15 +27,17 @@ class LoginActivity : AppCompatActivity()  {
 
         forgetPassword.setOnClickListener {
             val resetView = LayoutInflater.from(this).inflate(R.layout.layout_forget_password, null)
-            val resetViewBuilder = AlertDialog.Builder(this, R.style.PopUpWindow).setView(resetView).setTitle("Material Type Name")
+            val resetViewBuilder = AlertDialog.Builder(this, R.style.PopUpWindow).setView(resetView).setTitle("Reset Password")
             //show dialog
             val displayDialog = resetViewBuilder.show()
 
-//            //Cancel
-//            resetView.btn_cancel_reset.setOnClickListener{
-//                displayDialog.dismiss()
-//                Toast.makeText(this,"Cancelled", Toast.LENGTH_LONG).show()
-//            }
+//            resetView.setBack
+//            setBackgroundDrawable(ColorDrawable(android.R.color.transparent))
+            //Cancel
+            resetView.btn_cancel_reset.setOnClickListener{
+                displayDialog.dismiss()
+                Toast.makeText(this,"Cancelled", Toast.LENGTH_LONG).show()
+            }
 //            //send reset email
 //            resetView.btn_send_reset_email.setOnClickListener{
 //
