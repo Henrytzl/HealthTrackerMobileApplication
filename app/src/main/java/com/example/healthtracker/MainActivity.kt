cfa4674610e166f95fab7ca1a14c3e23e84238a0
KJ <kangjing324@live.com>
2021-10-16 02:14:57 +0800

    KJ update

--- app/src/main/java/com/example/healthtracker/MainActivity.kt
+++ app/src/main/java/com/example/healthtracker/MainActivity.kt
@@ -1,13 +1,52 @@
 package com.example.healthtracker
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.healthtracker.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.mProfile -> {
+
+                }
+                R.id.mFAQ -> {
+
+                }
+                R.id.mFeedback -> {
+
+                }
+                R.id.mHelp -> {
+
+                }
+                R.id.mAboutUs -> {
+
+                }
+                R.id.mLogout -> {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                }
+            }
+            true
+        }
     }
 
 }
