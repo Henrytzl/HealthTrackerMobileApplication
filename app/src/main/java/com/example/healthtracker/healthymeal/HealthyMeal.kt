b729de0a4937f2788b5c36e4925cee48b7f0e8eb
KJ <kangjing324@live.com>
2021-10-18 18:05:00 +0800

    KJ update

--- app/src/main/java/com/example/healthtracker/healthymeal/HealthyMeal.kt
+++ app/src/main/java/com/example/healthtracker/healthymeal/HealthyMeal.kt
@@ -1,23 +1,22 @@
-package com.example.healthtracker
+package com.example.healthtracker.healthymeal
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.healthtracker.healthymeal.HealthyMeal
+import com.example.healthtracker.MainActivity
+import com.example.healthtracker.R
 import com.example.healthtracker.login.LoginActivity
-import com.example.healthtracker.reminder.Reminder
 import kotlinx.android.synthetic.main.activity_main.*
 
-
-class MainActivity : AppCompatActivity() {
+class HealthyMeal : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_healthy_meal)
 
         setSupportActionBar(toolbar)
         supportActionBar?.title = ""
@@ -29,6 +28,10 @@ class MainActivity : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener{
             when(it.itemId){
+                R.id.mHome -> {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }
                 R.id.mProfile -> {
 
                 }
@@ -52,12 +55,9 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        btnReminder.setOnClickListener {
-            startActivity(Intent(this, Reminder::class.java))
-        }
-
-        btnHealthMeal.setOnClickListener {
-            startActivity(Intent(this, HealthyMeal::class.java))
+        imageHome.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
 
@@ -65,15 +65,6 @@ class MainActivity : AppCompatActivity() {
         if (toggle.onOptionsItemSelected(item)) {
             return true
         }
-//        if (item != null && item.getItemId() == android.R.id.home) {
-//            if (drawerLayout.isDrawerOpen(Gravity.RIGHT)) {
-//                drawerLayout.closeDrawer(Gravity.RIGHT);
-//            }
-//            else {
-//                drawerLayout.openDrawer(Gravity.RIGHT);
-//            }
-//        }
-        //return false
         return super.onOptionsItemSelected(item)
     }
 }
