6cb6c653507645d069a2f8c28193d2e07f5220e7
HENRYTZL <henrytzl2@hotmail.com>
2021-10-19 12:30:47 +0800

    Articles UI done

---
+++ app/src/main/java/com/example/healthtracker/Article4.kt
@@ -0,0 +1,19 @@
+package com.example.healthtracker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+
+
+class Article4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.article4)
+    }
+
+}
