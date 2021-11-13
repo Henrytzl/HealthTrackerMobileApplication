1a159dc1c33bbbaf6deeca3fe5dc8fce9945f04b
HENRYTZL <henrytzl2@hotmail.com>
2021-11-13 12:25:38 +0800

    henry update

--- app/src/main/java/com/example/healthtracker/AuthorisedUser.kt
+++ app/src/main/java/com/example/healthtracker/AuthorisedUser.kt
@@ -1,27 +1,48 @@
 package com.example.healthtracker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.healthtracker.dataclass.User
+import com.example.healthtracker.login.LoginActivity
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header.view.*
+import kotlinx.android.synthetic.main.user_profile.*
+import kotlinx.android.synthetic.main.user_profile.autoCompleteTextViewGender
+import kotlinx.android.synthetic.main.user_profile.textInputLayoutAge
+import kotlinx.android.synthetic.main.user_profile.textInputLayoutGender
+import kotlinx.android.synthetic.main.user_profile.toolbar
 
 
 class AuthorisedUser : AppCompatActivity() {
+    private lateinit var authentication: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private lateinit var userID: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_profile)
 
-        disableEdit()
+        //Tool Bar
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        db = FirebaseFirestore.getInstance()
+        authentication = FirebaseAuth.getInstance()
 
+        //disableEdit()
 
         val usernameTxt = findViewById<TextInputEditText>(R.id.textInputEditTextUsername)
         val emailTxt = findViewById<TextInputEditText>(R.id.textInputEditTextEmail)
@@ -35,21 +56,45 @@ class AuthorisedUser : AppCompatActivity() {
 
         //gender dropdown list
         val genderDdl = resources.getStringArray(R.array.Gender)
-        val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextViewGender)
         val adapter = ArrayAdapter(applicationContext, R.layout.dropdown_item, genderDdl)
 
-        autoCompleteTextView.setAdapter(adapter)
+        autoCompleteTextViewGender.setAdapter(adapter)
+
+        //profile form
+        val db = FirebaseFirestore.getInstance()
+        val emailDocRef = db.collection("User").document(userID)
+        emailDocRef
+            .get()
+            .addOnSuccessListener { email ->
+                if (email != null) {
+//                    textInputEditTextEmail.text = email.getString("email")
+                }
+            }
+//        val caloriesDocRef = db.collection("User").document(userID)
+//        caloriesDocRef.get().addOnSuccessListener { kcal ->
+//            if(kcal != null){
+//                textInputEditTextCalories.text.toString().toInt() = kcal.get("calories").toString().toInt()
+//            }
+//        }
 
         //save button
-        val saveBtn = findViewById<Button>(R.id.buttonSave)
-
-        saveBtn.setOnClickListener {
+        buttonSave.setOnClickListener {
             val alertDialog = AlertDialog.Builder(this)
             alertDialog.setTitle("Alert!!!")
             alertDialog.setMessage("Do you want to save your profile details ?")
             alertDialog.setCancelable(true)
             alertDialog.setPositiveButton("Yes") { dialog, id ->
-                Toast.makeText(this, "Saved Successfully!!!", Toast.LENGTH_SHORT).show()
+                val db = FirebaseFirestore.getInstance()
+                val documentRef = db.collection("User").document(userID)
+                val user = User(textInputEditTextUsername.text.toString(),  textInputEditTextAge.text.toString().toInt(), autoCompleteTextViewGender.text.toString(), 0, "")
+                documentRef
+                    .set(user)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Saved Successfully!!!", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "Failed to save", Toast.LENGTH_SHORT).show()
+                    }
             }
             alertDialog.setNegativeButton("No") { dialog, id ->
                 dialog.dismiss()
@@ -75,33 +120,33 @@ class AuthorisedUser : AppCompatActivity() {
     }
 
     private fun disableEdit() {
-        val username = findViewById<TextInputLayout>(R.id.textInputLayoutUsername)
-        val email = findViewById<TextInputLayout>(R.id.textInputLayoutEmail)
-        val age = findViewById<TextInputLayout>(R.id.textInputLayoutAge)
-        val calories = findViewById<TextInputLayout>(R.id.textInputLayoutCalories)
-        val gender = findViewById<TextInputLayout>(R.id.textInputLayoutGender)
-        val current = findViewById<TextInputLayout>(R.id.textInputLayoutCurrentPassword)
-        val new = findViewById<TextInputLayout>(R.id.textInputLayoutNewPassword)
-
-        username.isEnabled = false
-        email.isEnabled = false
-        age.isEnabled = false
-        calories.isEnabled = false
-        gender.isEnabled = false
-        current.isEnabled = false
-        new.isEnabled = false
+        textInputLayoutUsername.isEnabled = false
+        textInputLayoutEmail.isEnabled = false
+        textInputLayoutAge.isEnabled = false
+        textInputLayoutCalories.isEnabled = false
+        textInputLayoutGender.isEnabled = false
+        textInputLayoutCurrentPassword.isEnabled = false
+        textInputLayoutNewPassword.isEnabled = false
     }
 
     private fun enableEdit() {
-        val age = findViewById<TextInputLayout>(R.id.textInputLayoutAge)
-        val gender = findViewById<TextInputLayout>(R.id.textInputLayoutGender)
-        val current = findViewById<TextInputLayout>(R.id.textInputLayoutCurrentPassword)
-        val new = findViewById<TextInputLayout>(R.id.textInputLayoutNewPassword)
-
-        age.isEnabled = true
-        gender.isEnabled = true
-        current.isEnabled = true
-        new.isEnabled = true
+        textInputLayoutUsername.isEnabled = true
+        textInputLayoutAge.isEnabled = true
+        textInputLayoutGender.isEnabled = true
+        textInputLayoutCurrentPassword.isEnabled = true
+        textInputLayoutNewPassword.isEnabled = true
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        // Get current User id and email
+        if (authentication.currentUser != null) {
+            userID = authentication.currentUser!!.uid
+        } else {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+            finishAffinity()
+        }
+    }
 }
